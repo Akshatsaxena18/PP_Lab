017def25e91d7177a106de5f982e014b6500114c
Akshatsaxena18 <akshatsaxenaanp.as@gmail.com>
2023-01-15 21:11:47 +0530

    Update q6.c

--- openmp/q6.c
+++ openmp/q6.c
@@ -3,6 +3,14 @@
 #include <omp.h>
 // compile using : gcc -fopenmp prgm6.c vect_addn_func.c
 // to run ./a.out in ubuntu and a in windows
+void vector_addn_parallel(int n, int *res_vector, int *vector_a, int *vector_b)
+{
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+        res_vector[i] = vector_a[i] + vector_b[i];
+    }
+}
 void main()
 {
     int n = 100000;
